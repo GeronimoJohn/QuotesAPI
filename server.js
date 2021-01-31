f7d76616ca3239d87e64d6c26594fb38250a2cf7
GeronimoJohn <johnageronimo1@gmail.com>
2021-02-01 11:32:00 +1300

    Set up the port connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const { request } = require("express");
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+// app.get("/index", (req, res, next) => {
+//   res.send(qoutes);
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
